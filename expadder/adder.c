53d4306e82c1e87ab124131a2cc77585f7f6282f
mistryalok <alokn73@gmail.com>
2016-04-10 10:18:23 +0530

    Experiment

--- expadder/adder.c
+++ expadder/adder.c
@@ -1,6 +1,6 @@
    /*
     AUTHOR: Alok Mistry
-    PROGRAM: Character Device Driver
+    PROGRAM: Character Device Driver as an Adder
     VERSION: 4.0
     */
      
@@ -20,16 +20,18 @@
 //    #include <asm/system.h>
     #include <asm/uaccess.h>
      
-    #define DEVICE_NAME "mydevice"
-    #define CLASS_NAME "AlokMistry"
-    #define BUFFER_SIZE 1024
+    #define DEVICE_NAME "ALU"
+    #define DEVICE_CLASS "chardevice"
+    #define DRIVER_NAME "adder"
+    //#define CLASS_NAME "AlokMistry"
+    //#define BUFFER_SIZE 1024
      
    /*
     * License and other infos.
     */
-    MODULE_LICENSE("Dual BSD/GPL");
+    MODULE_LICENSE("GPL");
     MODULE_AUTHOR("Alok Mistry");
-    MODULE_DESCRIPTION("A simple character device driver.");
+    MODULE_DESCRIPTION("An adder with some extra stuff!.");
     MODULE_SUPPORTED_DEVICE(DEVICE_NAME);
      
     /* Current len in read and write */
@@ -39,20 +41,24 @@
     static int operand_2=0;
     static int result=0;
     static int resp_ready=0;
-	/* to have some memory to write and rd */
+    
+    /* to have some memory to write and rd */
     static char ker_buf[100];
     
     static dev_t first;
+    /* The chardevice struct in new way */
     static struct cdev c_dev;
     
+    /* Creating some class  */
     static struct class *driver_class=NULL;
     static struct device *ourDevice;
+    
     /* our dev init and exit modules */
     static int device_init(void);
     static void device_exit(void);
     
-    
-    static int device_open(struct inode *, 	struct file *);
+    /* Some routine declarations */
+    static int device_open(struct inode *,struct file *);
     static int device_release(struct inode *, struct file *);
     static ssize_t device_read(struct file *, char *, size_t, loff_t *);
     static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
@@ -60,16 +66,9 @@
     /* defining that they are actually ! */
     module_init(device_init);
     module_exit(device_exit);
-    
-    /*
-     * Define some attribuites to do something
-     */
-    //static DEVICE_ATTR(parCrtl,S_IWUSR,NULL,writeSomeAttr);
-    //static DEVICE_ATTR(isBusy,S_IRUGO,readSomeAttr,NULL);
 
     /* 
      *	file operations that we can do with our device
-     *	
      */
     static struct file_operations fops = {
         .read = device_read,
@@ -78,112 +77,104 @@
         .release = device_release
     };
 
-    /* Mentioning major and minor here */     
-    //static int device_major = 60;
-    //static int device_opend = 0;
-    //static char device_buffer[BUFFER_SIZE];
-    //static char *buff_rptr;
-    //static char *buff_wptr;
-     
-//    module_param(device_major, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
- //   MODULE_PARM_DESC(device_major, DEVICE_NAME " major number");
-     
     
-/*
- *Reading the file
- */
+    /*
+    *Reading the file
+    */
 
-static ssize_t readSomeAttr(struct device *dev,
-			    struct device_attribute *attr,char *buf)
-{
-  int isBusyResp=0;
-  sprintf(buf,"Busy is %d", isBusyResp);
-  return strlen(buf)+1;
-}
+    static ssize_t readSomeAttr(struct device *dev,
+				struct device_attribute *attr,char *buf)
+    {
+      int isBusyResp=0;
+      sprintf(buf,"Busy is %d", isBusyResp);
+      return strlen(buf)+1;
+    }
 
 
 
-/*
- * Writing the attribuites
- */
-static ssize_t writeSomeAttr(struct device *dev,struct device_attribute *attr,
-			     const char *buf,size_t count)
-{
-  printk(KERN_ALERT "we have got %d bytes",(int)count);
-  if(count>0) {
-      if(buf[0]=='=') {
-	printk(KERN_ALERT "Doing calculation");
-	resp_ready=1;
-	switch (math_operation) {
-	  case '+':
-	  {
-	    result=operand_1+operand_2;
-	    break;
+    /*
+    * Writing the attribuites
+    */
+    static ssize_t writeSomeAttr(struct device *dev,
+				struct device_attribute *attr,
+				const char *buf,size_t count)
+    {
+      printk(KERN_ALERT "we have got %d bytes",(int)count);
+      if(count>0) {
+	  if(buf[0]=='=') {
+	    printk(KERN_ALERT "Doing calculation");
+	    resp_ready=1;
+	    switch (math_operation) {
+	      case '+':
+	      {
+		result=operand_1+operand_2;
+		break;
+	      }
+	      case '-':
+	      {
+		result=operand_1-operand_2;
+		break;
+	      }
+	      default:
+		result=0;
+	    }
+	    printk(KERN_ALERT "The answer is %d",result);
 	  }
-	  case '-':
-	  {
-	    result=operand_1-operand_2;
-	    break;
-	  }
-	  default:
-	     result=0;
-	}
-	printk(KERN_ALERT "The answer is %d",result);
       }
-  }
-  return count;
-}
+      printk(KERN_ALERT "We have got %d and %d",buf[0],buf[1]);
+      return count;
+    }
 	  
     
-static DEVICE_ATTR(parCrtl,S_IWUSR,NULL,writeSomeAttr);
-static DEVICE_ATTR(isBusy,S_IRUGO,readSomeAttr,NULL);
+    static DEVICE_ATTR(parCrtl,S_IWUSR,NULL,writeSomeAttr);
+    static DEVICE_ATTR(isBusy,S_IRUGO,readSomeAttr,NULL);
 
-/*
- * Registering my device with new method
- */
- static int __init device_init(void) /* Constructor */
-{
-      printk(KERN_ALERT "INIT Hi, Your device is registered");
-      if (alloc_chrdev_region(&first, 0,1, "alok") < 0) {
-	return -1;
-      }
-      
-      if ((driver_class = class_create(THIS_MODULE, "myclass")) == NULL) {
-	unregister_chrdev_region(first, 1);
-	return -1;
-      }
-      ourDevice=device_create(driver_class,NULL,first,NULL,"mydevice");
-	
-      if (ourDevice == NULL) {
-	class_destroy(driver_class);
-	unregister_chrdev_region(first, 1);
-	return -1;
-      }
-      
-	cdev_init(&c_dev, &fops);
-	
-      if (cdev_add(&c_dev, first, 1) == -1) {
-	device_destroy(driver_class, first);
-	class_destroy(driver_class);
-	unregister_chrdev_region(first, 1);
-	return -1;
-      }
-      
-      
-      /*
-       * Lets create some attribute on /sys/class/CLASSNAME/mydevice/
-       */
-      if(device_create_file(ourDevice,&dev_attr_parCrtl)<0) {
-	printk(KERN_ALERT "Failed attr creation");
-	return -1;
-      }
-      if(device_create_file(ourDevice,&dev_attr_isBusy)<0) {
-	printk(KERN_ALERT "Failed attr creation");
-	return -1;
-      }
-      
-      return 0;
-}
+    /*
+    * Registering my device with new method, kind of constructor
+    */
+    static int __init device_init(void) 
+    {
+	  printk(KERN_ALERT "device_init: Your device has got registered\n");
+	  if (alloc_chrdev_region(&first, 0,1, DRIVER_NAME) < 0) {
+	    return -1;
+	  }
+	  
+	  if ((driver_class = class_create(THIS_MODULE, DEVICE_CLASS)) == NULL) {
+	    unregister_chrdev_region(first, 1);
+	    return -1;
+	  }
+	  ourDevice=device_create(driver_class,NULL,first,NULL,DEVICE_NAME);
+	    
+	  if (ourDevice == NULL) {
+	    class_destroy(driver_class);
+	    unregister_chrdev_region(first, 1);
+	    return -1;
+	  }
+	  
+	    cdev_init(&c_dev, &fops);
+	    
+	  if (cdev_add(&c_dev, first, 1) == -1) {
+	    device_destroy(driver_class, first);
+	    class_destroy(driver_class);
+	    unregister_chrdev_region(first, 1);
+	    return -1;
+	  }
+	  
+	  
+	  /*
+	  * Lets create some attribute on /sys/class/CLASSNAME/mydevice/
+	  */
+	  if(device_create_file(ourDevice,&dev_attr_parCrtl)<0) {
+	    printk(KERN_ALERT "Failed attr creation");
+	    return -1;
+	  }
+	  if(device_create_file(ourDevice,&dev_attr_isBusy)<0) {
+	    printk(KERN_ALERT "Failed attr creation");
+	    return -1;
+	  }
+	  
+	  return 0;
+    }
 
 
      
@@ -229,7 +220,7 @@ static DEVICE_ATTR(isBusy,S_IRUGO,readSomeAttr,NULL);
 	resp_ready=0;
         //int bytes_read = strlen(buff_rptr);
         //if(bytes_read > length) bytes_read = length;
-        printk(KERN_ALERT "REAR I am reading it!");
+        printk(KERN_ALERT "READ I am reading it!");
 	int n;
 	n=sprintf(ker_buf,"%d\n",result);
 	copy_to_user(buff, ker_buf, n);
